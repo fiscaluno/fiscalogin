3d3b5ff062fefb87b5c547060c993aab13a35cd3
alexandre.naime <Tivit@1234!>
2018-05-24 19:27:41 -0300

    discard

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,38 @@ import "../style/style.css";
 class Header extends Component {
   constructor() {
     super();
-    this.state = { loginObject: [], title: 'ENTRAR' };
+    this.state = { loginObject: [], title: "ENTRAR" };
   }
 
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: "{your-app-id}",
+        cookie: true,
+        xfbml: true,
+        version: "{latest-api-version}"
+      });
 
+      FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }
 
   handleLoginRequest = e => {
     try {
       api.get().then(response => {
-        this.setState({ loginObject: response.data, title:'Ola, usuario' });
+        this.setState({ loginObject: response.data, title: "Ola, usuario" });
       });
     } catch (error) {
       console.log(error);
